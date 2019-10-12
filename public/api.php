ed2bd391cccd8bbe42256d76a8411b1123d9a014
Bogdan Nesterenko <bogdan.n@giantleaplab.com>
2019-10-12 17:28:41 +0300

    added processing action calls refactoring existing api classes - in progress

--- public/api.php
+++ public/api.php
@@ -4,6 +4,36 @@ require '../vendor/autoload.php';
 
 use Api\SitaApi_Airport;
 
+$actions = [
+    'weather' => [
+        'class' => 'SitaApi_Weather',
+        'method' => 'getWeather'
+    ],
+    'carbon' => [
+        'class' => 'CarbonApi',
+        'method' => 'getCarbonEmission'
+    ],
+];
+
 $client = new \GuzzleHttp\Client();
 
-SitaApi_Airport::getAirports($client);
+$actionName = !empty($_REQUEST['action']) ? $_REQUEST['action'] : false;
+
+if(!empty($actionName) && in_array($actionName, array_keys($actions))) {
+    $action = $actions[$actionName];
+    $className = '\Api\\' . $action['class'];
+    if (class_exists($className) && method_exists($className, $action['method'])) {
+        unset($_REQUEST['action']);
+        $params = array_map(function($param){
+            $parts = explode('_', $param);
+            if (count($parts) == 2) {
+                return $parts[0] . ucfirst($parts[1]);
+            }
+            return $param;
+        }, $_REQUEST);
+        $className::{$action['method']}($client, $params);
+    }
+}
+
+
+//SitaApi_Airport::getAirports($client);
